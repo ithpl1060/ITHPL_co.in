23a199d1aa4a0bf1dc41456aacf6baf23ad9aa8f
Dinesh Nikam <71932998+dinesh-nikam@users.noreply.github.com>
2026-09-04 15:17:37 +0530

    global css changes

--- app/Views/ui/appleheader.php
+++ app/Views/ui/appleheader.php
@@ -29,7 +29,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4" defer></script>
 
     <!-- Global Styles -->
-    <link rel="stylesheet" href="<?= base_url('css/global.css') ?>" />
+    <link rel="stylesheet" href="<?= base_url('css/global.css') ?>?v=1.0.1" />
 
     <!-- Optional: Section for Per-Page Styles -->
     <?= $this->renderSection('headAssets') ?>
